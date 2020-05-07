b64eae2fc8d134a0d31f5be003e8911efd887beb
vngarg <sngargrsd@gmail.com>
2020-05-07 14:28:49 +0530

    Added rating and movie description

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:movie_finder/bloc/get_movie_videos.dart';
 import 'package:movie_finder/models/movie.dart';
 import 'package:movie_finder/models/video.dart';
@@ -97,6 +99,70 @@ class _MovieDetailScreenState extends State<MovieDetailScreen> {
                     ),
                   ),
                 ),
+                SliverPadding(
+                  padding: EdgeInsets.all(0.0),
+                  sliver: SliverList(
+                    delegate: SliverChildListDelegate([
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              movie.rating.toString(),
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 5.0,
+                            ),
+                            RatingBar(
+                              itemSize: 8.0,
+                              initialRating: movie.rating / 2,
+                              minRating: 1,
+                              direction: Axis.horizontal,
+                              itemCount: 5,
+                              allowHalfRating: true,
+                              itemPadding:
+                                  EdgeInsets.symmetric(horizontal: 2.0),
+                              itemBuilder: (context, _) => Icon(
+                                EvaIcons.star,
+                                color: Style.Colors.secondColor,
+                              ),
+                              onRatingUpdate: (rating) {
+                                print(rating);
+                              },
+                            )
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 10.0, top: 20.0),
+                        child: Text(
+                          "OVERVIEW",
+                          style: TextStyle(
+                            color: Style.Colors.titleColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12.0,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 5.0,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: Text(movie.overview,
+                            style: TextStyle(
+                                color: Colors.white,
+                                height: 1.5,
+                                fontSize: 12.0)),
+                      ),
+                    ]),
+                  ),
+                )
               ]);
         },
       ),
